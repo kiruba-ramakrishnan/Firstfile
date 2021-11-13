b074cef48f7718097f8540fae0bf6814ed236bbf
kiruba-ramakrishnan <kirubaece99@gmail.com>
2021-11-13 17:48:34 +0530

    udate my code

---
+++ src/test/java/org/hcl/test/kiruba.java
@@ -0,0 +1,17 @@
+package org.hcl.test;
+
+public class kiruba {
+	public void javacourse() {
+		// TODO Auto-generated method stub
+
+	}
+	public void pythoncourse() {
+		// TODO Auto-generated method stub
+
+	}
+	public void awscourse() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
