b9fb1e1ae7f27fc63112f2de647d4d419be00834
kiruba-ramakrishnan <kirubaece99@gmail.com>
2021-11-13 13:09:31 +0530

    firstcode is pushed

---
+++ src/test/java/org/hcl/test/ClassA.java
@@ -0,0 +1,17 @@
+package org.hcl.test;
+
+public class ClassA {
+	public void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	public void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	public void method3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
